e7386cf70b3967e830cc74757158fbb7216e430f
Shiv Prasad <shivprasad@Shivs-MacBook-Pro.local>
2023-06-30 18:21:44 +0530

    Image optimization and responsive memes

--- src/components/ImageList/ImageListItemComp.js
+++ src/components/ImageList/ImageListItemComp.js
@@ -1,33 +1,37 @@
-import { ImageListItem, ImageList } from '@mui/material';
-import Image from 'next/image';
-import React from 'react'
+import { ImageListItem, ImageList, useMediaQuery } from "@mui/material";
+import Image from "next/image";
+import React, { useEffect } from "react";
 
-const ImageListItemComp = ({itemData}) => {
+const ImageListItemComp = ({ itemData }) => {
+  const sxScreen = useMediaQuery("(max-width:480px)");
+  const mdScreen = useMediaQuery("(max-width:720px)");
+  const lgScreen = useMediaQuery("(max-width:1080px)");
 
-    return (
-        <ImageList 
-          // sx={{ width: 500, height: 450 }}
-          
-          variant="quilted"
-          cols={5}
-          
-          // rowHeight={121}
-        >
-          {itemData.map((item, key) => (
-            <ImageListItem key={key} >
-              <img
-              style = {{width:300,
-              height:300,margin : "20px"}}
+  let cols;
+  if (sxScreen) {
+    cols = 1;
+  } else if (mdScreen) {
+    cols = 2;
+  } else if (lgScreen) {
+    cols = 3;
+  } else {
+    cols = 5;
+  }
 
-                src={`/memes/${item} `}
-                alt={"Memes"}
-                // loading="lazy"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      
-  )
-}
+  return (
+    <ImageList variant="quilted" cols={cols}>
+      {itemData.map((item, key) => (
+        <ImageListItem key={key}>
+          <img
+            style={{ width: 300, height: 300, margin: "20px" }}
+            src={`/memes/${item} `}
+            alt={"Memes"}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+};
 
-export default ImageListItemComp
+export default ImageListItemComp;
