f6b62e3395ed111028d5d2c39e0cbaaeb38dbb49
Shiv Prasad <shivprasad@Shivs-MacBook-Pro.local>
2023-06-30 16:37:52 +0530

    Added Meme component

---
+++ src/components/MemeComponent/MemeComponent.js
@@ -0,0 +1,65 @@
+
+import React, { useEffect, useState } from 'react'
+import LoadMoreButton from '@/components/Button/LoadMoreButton';
+import ImageList from '@/components/ImageList/ImageListItemComp';
+import Image from 'next/image';
+
+const MemeComponent = () => {
+    const [fileNames, setFileNames] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
+    const [page, setPage] = useState(1)
+
+
+    const loadMoreFileNames = async () => {
+
+        setLoading(true)
+        setError(null)
+    
+    try {
+        const response = await fetch(`/api/memes?page=${page}`);
+      const data = await response.json();
+      setFileNames([...fileNames, ...data.fileNames]);
+      setPage(prevPage => prevPage + 1)
+        
+    } catch (error) {
+        setError(error)
+    }finally {
+        setLoading(false)
+    }
+      
+    };
+
+    useEffect(() => {
+        loadMoreFileNames();
+    },[])
+
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
+          return;
+        }
+        loadMoreFileNames();
+      };
+      
+      useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, [loading]);
+      
+  return (
+    <div style={{ width :"100%"}}>
+           <ImageList itemData={fileNames} />
+      {/* Render a "Load More" button */}
+      <div style={{ display:"flex", justifyContent:"center", alignItems:"center", marginBottom:"20px" }}>
+
+      {loading && <div>Loading </div>}
+      {error && <p>Error: {error.message}</p>}
+      </div>
+    
+  
+    
+   </div>
+  )
+}
+
+export default MemeComponent
